f3b883ac4bc1075c9a5fdcf2ec1126c061d76771
TranTPhuong <trantuanphuong19941994@gmail.com>
2016-02-25 21:37:37 +0700

    Add list of stocks on the view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   
   def my_portfolio
-    
+    @user_stocks = current_user.stocks
+    @user = current_user
   end
   
   def my_friends
-    
+    @friendships = current_user.friends
   end
 end
